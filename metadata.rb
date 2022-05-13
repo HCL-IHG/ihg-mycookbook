e27d2ed3053ea18e83055a00929f7e617584705c
Param <param135@gmail.com>
2022-05-13 02:04:37 +0000

    First working chef cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,3 @@ license 'All rights reserved'
 description 'Installs/Configures ihg-logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.134'
-depends 'ihg-stdjava'
-depends 'ihg-yumrepos'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+file '/etc/motd' do
+content 'This is the property ..'
+end
